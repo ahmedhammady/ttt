2b9e14d9c502f9cbbd9038cbc161629235b124fd
ahmedhammady <a.hammady1@gmail.com>
2017-09-25 16:45:39 +0100

    TestPull

--- ttt/src/App.js
+++ ttt/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-Let's goooooo        </p>
+Let's goooooo. It's a game now.    </p>
       </div>
     );
   }
