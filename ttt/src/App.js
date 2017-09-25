80a3e46c673ea04f9d49ce639c70a527d871b9b6
ahmedhammady <a.hammady1@gmail.com>
2017-09-25 16:32:14 +0100

    adding os

--- ttt/src/App.js
+++ ttt/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-Let's goooo        </p>
+Let's goooooo        </p>
       </div>
     );
   }
